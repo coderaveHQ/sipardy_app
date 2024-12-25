7961598c2c391b46bffaccbdceb8963176c34bd9
Florian Leeser <fleeser@coderave.dev>
2024-12-25 13:41:50 +0100

    chore: integrate Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:sipardy_app/core/utils/env.dart';
 import 'package:sipardy_app/src/app.dart';
 
-void main() {
-  runApp(const App());
+/// The main entry point for the application
+Future<void> main() async {
+
+  // Ensure that the widgets binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load the environment variables
+  await loadEnv();
+
+  // Initialize the Supabase client
+  await initializeSupabase();
+
+  // Run the application
+  runApp(
+    const ProviderScope(
+      child: App()
+    )
+  );
+}
+
+/// Loads the environment variables
+Future<void> loadEnv() async {
+  await dotenv.load();
+}
+
+/// Initializes the Supabase client
+Future<void> initializeSupabase() async {
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey,
+    authOptions: const FlutterAuthClientOptions(detectSessionInUri: false)
+  );
 }
