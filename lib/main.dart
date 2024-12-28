fb98ab43824acd30f2def4019ae94d93b7fab9bb
Florian Leeser <fleeser@coderave.dev>
2024-12-28 02:59:25 +0100

    chore: create Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:sipardy_app/core/utils/env.dart';
@@ -12,7 +13,10 @@ import 'package:sipardy_app/src/app.dart';
 Future<void> main() async {
 
   // Ensure that the widgets binding is initialized
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Show the splash screen
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Set the preferred orientations
   await SystemChrome.setPreferredOrientations([ DeviceOrientation.portraitUp ]);
@@ -29,6 +33,9 @@ Future<void> main() async {
       child: App()
     )
   );
+
+  // Remove the splash screen
+  FlutterNativeSplash.remove();
 }
 
 /// Loads the environment variables
