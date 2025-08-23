fcd3433b6f69dfb359c3e1383b288e10c9b50b4a
Florian Leeser <fleeser@coderave.dev>
2025-08-23 08:24:59 +0200

    test: Firebase Hosting workflow

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -47,6 +47,11 @@ Future<void> main() async {
 Future<void> initializeSupabase() async {
   const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
   const String supabaseKey = String.fromEnvironment(kDebugMode ? 'SUPABASE_ANON_KEY' : 'SUPABASE_PUBLISHABLE_KEY');
+  
+  print(kDebugMode);
+  print(supabaseUrl);
+  print(supabaseKey);
+  
   await Supabase.initialize(
     url: supabaseUrl,
     anonKey: supabaseKey,
